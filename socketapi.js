0d0085e033c48ee20c0338128c445579e2647283
andrew-bbq <awbennett@wisc.edu>
2021-02-26 01:06:00 -0600

    Finish prototype

--- socketapi.js
+++ socketapi.js
@@ -3,9 +3,20 @@ const socketapi = {
     io: io
 };
 
-// socket io logic goes here
-io.on("connection", (socket) => {
+let nextId = 0;
+let drawing = [];
 
+// socket io logic goes here
+io.on("connect", (socket) => {
+    socket.on("Draw", function(data) {
+        drawing.push(data.newLine);
+        nextId = data.id + 1;
+        socket.broadcast.emit("ReceiveDrawing", data);
+    });
+});
+
+io.on("connect", (socket) => {
+    socket.emit("InitialDrawing", {drawing: drawing, nextId: nextId});
 });
 
 module.exports = socketapi;
